0745d349820603af3d4f1ee5e8591c2c604be169
Фаррах Фаттахов <farrakhfattahov@gmail.com>
2025-11-12 20:54:06 +0300

    Libraries added. Window created

--- SkeletonAnimationTestAdventure/Code/main.cpp
+++ SkeletonAnimationTestAdventure/Code/main.cpp
@@ -1,3 +1,21 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 int main() {
-    return 0;
+    if (!glfwInit()) return -1;
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Test", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 }
