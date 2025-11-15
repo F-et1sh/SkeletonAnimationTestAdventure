a1f26e1f8e02ab123a441763afc8c53c0a54dcee
Фаррах Фаттахов <farrakhfattahov@gmail.com>
2025-11-15 22:26:08 +0300

    GLM added

--- SkeletonAnimationTestAdventure/Code/main.cpp
+++ SkeletonAnimationTestAdventure/Code/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <filesystem>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/vector_angle.hpp>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -9,35 +15,57 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "tiny_gltf.h"
 
+#include "Shader.hpp"
+
 int main() {
     if (!glfwInit()) return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     GLFWwindow* window = glfwCreateWindow(800, 600, "Test", NULL, NULL);
+    if (!window) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
     glfwMakeContextCurrent(window);
 
     if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
         return -1;
     }
 
+    glViewport(0, 0, 800, 600);
+
+    Shader shader("F:\\Windows\\Desktop\\SkeletonAnimationTestAdventure\\Files\\Shaders\\default.vert",
+                  "F:\\Windows\\Desktop\\SkeletonAnimationTestAdventure\\Files\\Shaders\\default.frag");
+
+    // Take care of all the light related things
+    glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+    glm::vec3 lightPos   = glm::vec3(0.5f, 0.5f, 0.5f);
+    glm::mat4 lightModel = glm::mat4(1.0f);
+    lightModel           = glm::translate(lightModel, lightPos);
+
+    shader.Activate();
+    glUniform4f(glGetUniformLocation(shader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+    glUniform3f(glGetUniformLocation(shader.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+
     tinygltf::Model       model;
     tinygltf::TinyGLTF    gltf_ctx;
     std::string           err;
     std::string           warn;
-    std::filesystem::path input_filename = L"F:\\Windows\\Desktop\\SkeletonAnimationTestAdventure\\Files\\Cube\\Cube.gltf";
+    std::filesystem::path input_filename = L"F:\\Windows\\Desktop\\SkeletonAnimationTestAdventure\\Files\\Models\\Cube\\Cube.gltf";
     std::string           ext            = input_filename.extension().string();
 
     gltf_ctx.SetStoreOriginalJSONForExtrasAndExtensions(false);
 
-    bool ret = false;
+    bool success = false;
     if (ext.compare("glb") == 0) {
-        std::cout << "Reading binary glTF" << std::endl;
-        // assume binary glTF.
-        ret = gltf_ctx.LoadBinaryFromFile(&model, &err, &warn, input_filename.string().c_str());
+        success = gltf_ctx.LoadBinaryFromFile(&model, &err, &warn, input_filename.string().c_str());
     }
     else {
-        std::cout << "Reading ASCII glTF" << std::endl;
-        // assume ascii glTF.
-        ret = gltf_ctx.LoadASCIIFromFile(&model, &err, &warn, input_filename.string().c_str());
+        success = gltf_ctx.LoadASCIIFromFile(&model, &err, &warn, input_filename.string().c_str());
     }
 
     if (!warn.empty()) {
@@ -48,7 +76,7 @@ int main() {
         printf("Err: %s\n", err.c_str());
     }
 
-    if (!ret) {
+    if (!success) {
         printf("Failed to parse glTF\n");
         return -1;
     }
