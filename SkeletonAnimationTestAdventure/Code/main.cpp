bbb3a38d129b56e31cd2ac4350f348dd6f1a074b
Фаррах Фаттахов <farrakhfattahov@gmail.com>
2025-11-15 14:25:41 +0300

    GLFW files changed. TinyGLTF, Json and STBI are added. Model loaded

--- SkeletonAnimationTestAdventure/Code/main.cpp
+++ SkeletonAnimationTestAdventure/Code/main.cpp
@@ -1,5 +1,13 @@
+#include <iostream>
+#include <filesystem>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "tiny_gltf.h"
 
 int main() {
     if (!glfwInit()) return -1;
@@ -11,7 +19,42 @@ int main() {
         return -1;
     }
 
+    tinygltf::Model       model;
+    tinygltf::TinyGLTF    gltf_ctx;
+    std::string           err;
+    std::string           warn;
+    std::filesystem::path input_filename = L"F:\\Windows\\Desktop\\SkeletonAnimationTestAdventure\\Files\\Cube\\Cube.gltf";
+    std::string           ext            = input_filename.extension().string();
+
+    gltf_ctx.SetStoreOriginalJSONForExtrasAndExtensions(false);
+
+    bool ret = false;
+    if (ext.compare("glb") == 0) {
+        std::cout << "Reading binary glTF" << std::endl;
+        // assume binary glTF.
+        ret = gltf_ctx.LoadBinaryFromFile(&model, &err, &warn, input_filename.string().c_str());
+    }
+    else {
+        std::cout << "Reading ASCII glTF" << std::endl;
+        // assume ascii glTF.
+        ret = gltf_ctx.LoadASCIIFromFile(&model, &err, &warn, input_filename.string().c_str());
+    }
+
+    if (!warn.empty()) {
+        printf("Warn: %s\n", warn.c_str());
+    }
+
+    if (!err.empty()) {
+        printf("Err: %s\n", err.c_str());
+    }
+
+    if (!ret) {
+        printf("Failed to parse glTF\n");
+        return -1;
+    }
+
     while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
         glfwPollEvents();
