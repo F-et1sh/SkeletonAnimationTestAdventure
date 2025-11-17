fccee07ce40c14e993b11375fbd4f05b2f0172ae
Фаррах Фаттахов <farrakhfattahov@gmail.com>
2025-11-17 19:23:43 +0300

    Shader done

---
+++ SkeletonAnimationTestAdventure/Code/Shader.cpp
@@ -0,0 +1,238 @@
+#include "Shader.hpp"
+
+Shader::~Shader() {
+    glDeleteProgram(m_Program);
+}
+
+void Shader::Initialize(const std::filesystem::path& file_path) {
+
+    this->createProgram();
+
+    const std::filesystem::path path = file_path;
+
+    const std::filesystem::path vertex_path   = path.wstring() + L".vert";
+    const std::filesystem::path fragment_path = path.wstring() + L".frag";
+
+    const bool vert_init = this->loadFromFile(vertex_path, Shader::VERTEX);
+    const bool frag_init = this->loadFromFile(fragment_path, Shader::FRAGMENT);
+
+    if (vert_init && frag_init) {
+        this->LinkShader();
+        return;
+    }
+
+    // if user will use path with an extension, program anyway can work
+    // Example : SomeShader.shader ==> SomeShader.shader.vert & SomeShader.shader.frag
+    // Else -> throw an error
+
+    if (path.has_extension())
+        std::runtime_error(std::string("\nFailed to Initialize a shader") +
+                           std::string("\nYou tried to use one path for both shaders (vertex and fragment)") +
+                           std::string("\nDelete the extension of the path") +
+                           std::string("\nSource Path : ") + path.string() +
+                           std::string("\nVertex Path : ") + vertex_path.string() +
+                           std::string("\nFragment Path : ") + fragment_path.string());
+}
+
+void Shader::Initialize(const std::filesystem::path& vert_path, const std::filesystem::path& frag_path) {
+
+    this->createProgram();
+
+    const std::filesystem::path vertex_path   = vert_path;
+    const std::filesystem::path fragment_path = frag_path;
+
+    bool vert_init = this->loadFromFile(vertex_path, Shader::VERTEX);
+    bool frag_init = this->loadFromFile(fragment_path, Shader::FRAGMENT);
+
+    if (vert_init && frag_init) {
+        this->linkShader();
+        return;
+    }
+
+    std::runtime_error(std::string("Failed to Initialize a shader") +
+                       std::string("\nVertex Path : ") + vertex_path.string() +
+                       std::string("\nFrament Path : ") + fragment_path.string());
+}
+
+void Shader::createProgram() noexcept {
+    m_Program = glCreateProgram();
+}
+
+bool Shader::loadFromSource(const char* source, const unsigned int& type) {
+    // Vertex or Fragment Shader
+    unsigned int shader = glCreateShader(type);
+
+    // Load Source to Shader
+    glShaderSource(shader, 1, &source, nullptr);
+    // Compile the Shader
+    glCompileShader(shader);
+    // Bind the Shader to the Program
+    glAttachShader(m_Program, shader);
+    // Delete the Shader
+    glDeleteShader(shader);
+
+    // Check Shader Errors
+    int result;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*) _malloca(length * sizeof(char));
+        glGetShaderInfoLog(shader, length, &length, message);
+
+        std::wcerr << "ERROR : Failed to Compile Shader\n"
+                   << message << std::endl
+                   << std::endl;
+
+        glDeleteShader(shader);
+
+        return false;
+    }
+
+    return true;
+}
+
+bool Shader::loadFromFile(const std::filesystem::path& path, const unsigned int& type) {
+    std::ifstream file(path);
+    if (!file.good()) {
+        std::wcerr << L"ERROR: Failed to open shader file\nPath : " << path.c_str() << std::endl
+                   << std::endl;
+        return false;
+    }
+
+    std::string source((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+    file.close();
+
+    if (source.empty()) {
+        std::wcerr << L"ERROR: Shader file is empty\nPath: " << path.c_str() << std::endl
+                   << std::endl;
+        return false;
+    }
+
+    return loadFromSource(source.c_str(), type);
+}
+
+void Shader::linkShader() {
+    glLinkProgram(m_Program);
+    glValidateProgram(m_Program);
+}
+
+inline void Shader::Bind() const noexcept {
+    glUseProgram(m_Program);
+}
+inline void Shader::Unbind() const noexcept {
+    glUseProgram(0);
+}
+
+#pragma region Legacy Uniform Setting
+
+// Methods with Uniform Setting
+#pragma region Set Uniforms
+
+void Shader::setUniformInt(const char* uniform_name, int load_uniform) const noexcept {
+    glUniform1i(glGetUniformLocation(this->m_Program, uniform_name), load_uniform);
+}
+void Shader::setUniformIntArray(const char* uniform_name, int* load_uniform, unsigned int count) const noexcept {
+    glUniform1iv(glGetUniformLocation(this->m_Program, uniform_name), count, load_uniform);
+}
+
+void Shader::setUniformFloat(const char* uniform_name, float load_uniform) const noexcept {
+    glUniform1f(glGetUniformLocation(this->m_Program, uniform_name), load_uniform);
+}
+void Shader::setUniformFloatArray(const char* uniform_name, float* load_uniform, unsigned int count) const noexcept {
+    glUniform1fv(glGetUniformLocation(this->m_Program, uniform_name), count, load_uniform);
+}
+
+void Shader::setUniformVec2(const char* uniform_name, glm::vec2 load_uniform) const noexcept {
+    glUniform2f(glGetUniformLocation(this->m_Program, uniform_name), load_uniform.x, load_uniform.y);
+}
+void Shader::setUniformVec2Array(const char* uniform_name, glm::vec2* load_uniform, unsigned int count) const noexcept {
+    glUniform2fv(glGetUniformLocation(this->m_Program, uniform_name), count, &load_uniform[0].x);
+}
+
+void Shader::setUniformVec3(const char* uniform_name, glm::vec3 load_uniform) const noexcept {
+    glUniform3f(glGetUniformLocation(this->m_Program, uniform_name), load_uniform.x, load_uniform.y, load_uniform.z);
+}
+void Shader::setUniformVec3Array(const char* uniform_name, glm::vec3* load_uniform, unsigned int count) const noexcept {
+    glUniform3fv(glGetUniformLocation(this->m_Program, uniform_name), count, &load_uniform[0].x);
+}
+
+void Shader::setUniformVec4(const char* uniform_name, glm::vec4 load_uniform) const noexcept {
+    glUniform4f(glGetUniformLocation(this->m_Program, uniform_name), load_uniform.x, load_uniform.y, load_uniform.z, load_uniform.w);
+}
+void Shader::setUniformVec4Array(const char* uniform_name, glm::vec4* load_uniform, unsigned int count) const noexcept {
+    glUniform4fv(glGetUniformLocation(this->m_Program, uniform_name), count, &load_uniform[0].x);
+}
+
+void Shader::setUniformMat3(const char* uniform_name, glm::mat3 load_uniform) const noexcept {
+    glUniformMatrix3fv(glGetUniformLocation(this->m_Program, uniform_name), 1, GL_FALSE, glm::value_ptr(load_uniform));
+}
+void Shader::setUniformMat3Array(const char* uniform_name, glm::mat3* load_uniform, unsigned int count) const noexcept {
+    glUniformMatrix3fv(glGetUniformLocation(this->m_Program, uniform_name), count, GL_FALSE, glm::value_ptr(load_uniform[0]));
+}
+
+void Shader::setUniformMat4(const char* uniform_name, glm::mat4 load_uniform) const noexcept {
+    glUniformMatrix4fv(glGetUniformLocation(this->m_Program, uniform_name), 1, GL_FALSE, glm::value_ptr(load_uniform));
+}
+void Shader::setUniformMat4Array(const char* uniform_name, glm::mat4* load_uniform, unsigned int count) const noexcept {
+    glUniformMatrix4fv(glGetUniformLocation(this->m_Program, uniform_name), count, GL_FALSE, glm::value_ptr(load_uniform[0]));
+}
+
+#pragma endregion
+
+// Static Methods with Uniform Setting
+#pragma region Set Uniforms
+
+void Shader::setUniformInt(unsigned int shaderRef, const char* uniform_name, int load_uniform) noexcept {
+    glUniform1i(glGetUniformLocation(shaderRef, uniform_name), load_uniform);
+}
+void Shader::setUniformIntArray(unsigned int shaderRef, const char* uniform_name, int* load_uniform, unsigned int count) noexcept {
+    glUniform1iv(glGetUniformLocation(shaderRef, uniform_name), count, load_uniform);
+}
+
+void Shader::setUniformFloat(unsigned int shaderRef, const char* uniform_name, float load_uniform) noexcept {
+    glUniform1f(glGetUniformLocation(shaderRef, uniform_name), load_uniform);
+}
+void Shader::setUniformFloatArray(unsigned int shaderRef, const char* uniform_name, float* load_uniform, unsigned int count) noexcept {
+    glUniform1fv(glGetUniformLocation(shaderRef, uniform_name), count, load_uniform);
+}
+
+void Shader::setUniformVec2(unsigned int shaderRef, const char* uniform_name, const glm::vec2& load_uniform) noexcept {
+    glUniform2f(glGetUniformLocation(shaderRef, uniform_name), load_uniform.x, load_uniform.y);
+}
+void Shader::setUniformVec2Array(unsigned int shaderRef, const char* uniform_name, glm::vec2* load_uniform, unsigned int count) noexcept {
+    glUniform2fv(glGetUniformLocation(shaderRef, uniform_name), count, &load_uniform[0].x);
+}
+
+void Shader::setUniformVec3(unsigned int shaderRef, const char* uniform_name, const glm::vec3& load_uniform) noexcept {
+    glUniform3f(glGetUniformLocation(shaderRef, uniform_name), load_uniform.x, load_uniform.y, load_uniform.z);
+}
+void Shader::setUniformVec3Array(unsigned int shaderRef, const char* uniform_name, glm::vec3* load_uniform, unsigned int count) noexcept {
+    glUniform3fv(glGetUniformLocation(shaderRef, uniform_name), count, &load_uniform[0].x);
+}
+
+void Shader::setUniformVec4(unsigned int shaderRef, const char* uniform_name, const glm::vec4& load_uniform) noexcept {
+    glUniform4f(glGetUniformLocation(shaderRef, uniform_name), load_uniform.x, load_uniform.y, load_uniform.z, load_uniform.w);
+}
+void Shader::setUniformVec4Array(unsigned int shaderRef, const char* uniform_name, glm::vec4* load_uniform, unsigned int count) noexcept {
+    glUniform4fv(glGetUniformLocation(shaderRef, uniform_name), count, &load_uniform[0].x);
+}
+
+void Shader::setUniformMat3(unsigned int shaderRef, const char* uniform_name, const glm::mat3& load_uniform) noexcept {
+    glUniformMatrix3fv(glGetUniformLocation(shaderRef, uniform_name), 1, GL_FALSE, glm::value_ptr(load_uniform));
+}
+void Shader::setUniformMat3Array(unsigned int shaderRef, const char* uniform_name, glm::mat3* load_uniform, unsigned int count) noexcept {
+    glUniformMatrix3fv(glGetUniformLocation(shaderRef, uniform_name), count, GL_FALSE, glm::value_ptr(load_uniform[0]));
+}
+
+void Shader::setUniformMat4(unsigned int shaderRef, const char* uniform_name, const glm::mat4& load_uniform) noexcept {
+    glUniformMatrix4fv(glGetUniformLocation(shaderRef, uniform_name), 1, GL_FALSE, glm::value_ptr(load_uniform));
+}
+void Shader::setUniformMat4Array(unsigned int shaderRef, const char* uniform_name, glm::mat4* load_uniform, unsigned int count) noexcept {
+    glUniformMatrix4fv(glGetUniformLocation(shaderRef, uniform_name), count, GL_FALSE, glm::value_ptr(load_uniform[0]));
+}
+
+#pragma endregion
+
+#pragma endregion
---
+++ SkeletonAnimationTestAdventure/Code/Shader.hpp
@@ -0,0 +1,111 @@
+#pragma once
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/vector_angle.hpp>
+
+#include <glad/glad.h>
+
+class Shader {
+public:
+    Shader() = default;
+    ~Shader();
+
+    enum {
+        VERTEX   = 0x8B31,
+        FRAGMENT = 0x8B30
+    };
+
+    // Initialize the shader program with one path for both shaders ( vertex and fragment )
+    // don't use an extension here
+    void Initialize(const std::filesystem::path& file_path);
+    void Initialize(const std::filesystem::path& vert_path, const std::filesystem::path& frag_path);
+
+    inline void Bind() const noexcept;
+    inline void Unbind() const noexcept;
+
+    // Get Shader Program Refecence
+    inline const unsigned int& reference() const noexcept { return m_Program; }
+
+private:
+    void createProgram() noexcept;
+
+    bool loadFromSource(const char* source, const unsigned int& type);
+    bool loadFromFile(const std::filesystem::path& path, const unsigned int& type);
+
+    void linkShader();
+
+    // Reference of this Shader Program
+    GLuint m_Program = 0;
+
+public:
+    // Legacy Methods with Uniform Setting
+#pragma region Set Uniforms
+
+    // Set int [ ACTIVATE SHADER FIRST ]
+    inline void setUniformInt(const char* uniform_name, int load_uniform) const noexcept;
+    // Set int Array [ ACTIVATE SHADER FIRST ]
+    inline void setUniformIntArray(const char* uniform_name, int* load_uniform, unsigned int count) const noexcept;
+
+    // Set float [ ACTIVATE SHADER FIRST ]
+    inline void setUniformFloat(const char* uniform_name, float load_uniform) const noexcept;
+    // Set float Array [ ACTIVATE SHADER FIRST ]
+    inline void setUniformFloatArray(const char* uniform_name, float* load_uniform, unsigned int count) const noexcept;
+
+    // Set vec2 [ ACTIVATE SHADER FIRST ]
+    inline void setUniformVec2(const char* uniform_name, glm::vec2 load_uniform) const noexcept;
+    // Set vec2 Array [ ACTIVATE SHADER FIRST ]
+    inline void setUniformVec2Array(const char* uniform_name, glm::vec2* load_uniform, unsigned int count) const noexcept;
+    // Set vec3 [ ACTIVATE SHADER FIRST ]
+    inline void setUniformVec3(const char* uniform_name, glm::vec3 load_uniform) const noexcept;
+    // Set vec3 [ ACTIVATE SHADER FIRST ]
+    inline void setUniformVec3Array(const char* uniform_name, glm::vec3* load_uniform, unsigned int count) const noexcept;
+    // Set vec4 [ ACTIVATE SHADER FIRST ]
+    inline void setUniformVec4(const char* uniform_name, glm::vec4 load_uniform) const noexcept;
+    // Set vec4 Array [ ACTIVATE SHADER FIRST ]
+    inline void setUniformVec4Array(const char* uniform_name, glm::vec4* load_uniform, unsigned int count) const noexcept;
+
+    // Set mat3 [ ACTIVATE SHADER FIRST ]
+    inline void setUniformMat3(const char* uniform_name, glm::mat3 load_uniform) const noexcept;
+    // Set mat3 Array [ ACTIVATE SHADER FIRST ]
+    inline void setUniformMat3Array(const char* uniform_name, glm::mat3* load_uniform, unsigned int count) const noexcept;
+
+    // Set mat4 [ ACTIVATE SHADER FIRST ]
+    inline void setUniformMat4(const char* uniform_name, glm::mat4 load_uniform) const noexcept;
+    // Set mat4 Array [ ACTIVATE SHADER FIRST ]
+    inline void setUniformMat4Array(const char* uniform_name, glm::mat4* load_uniform, unsigned int count) const noexcept;
+
+#pragma endregion
+
+    // Static Legacy Methods with Uniform Setting
+#pragma region Set Uniforms without Existing Shader
+
+    static inline void setUniformInt(unsigned int shaderRef, const char* uniform_name, int load_uniform) noexcept;
+    static inline void setUniformIntArray(unsigned int shaderRef, const char* uniform_name, int* load_uniform, unsigned int count) noexcept;
+
+    static inline void setUniformFloat(unsigned int shaderRef, const char* uniform_name, float load_uniform) noexcept;
+    static inline void setUniformFloatArray(unsigned int shaderRef, const char* uniform_name, float* load_uniform, unsigned int count) noexcept;
+
+    static inline void setUniformVec2(unsigned int shaderRef, const char* uniform_name, const glm::vec2& load_uniform) noexcept;
+    static inline void setUniformVec2Array(unsigned int shaderRef, const char* uniform_name, glm::vec2* load_uniform, unsigned int count) noexcept;
+
+    static inline void setUniformVec3(unsigned int shaderRef, const char* uniform_name, const glm::vec3& load_uniform) noexcept;
+    static inline void setUniformVec3Array(unsigned int shaderRef, const char* uniform_name, glm::vec3* load_uniform, unsigned int count) noexcept;
+
+    static inline void setUniformVec4(unsigned int shaderRef, const char* uniform_name, const glm::vec4& load_uniform) noexcept;
+    static inline void setUniformVec4Array(unsigned int shaderRef, const char* uniform_name, glm::vec4* load_uniform, unsigned int count) noexcept;
+
+    static inline void setUniformMat3(unsigned int shaderRef, const char* uniform_name, const glm::mat3& load_uniform) noexcept;
+    static inline void setUniformMat3Array(unsigned int shaderRef, const char* uniform_name, glm::mat3* load_uniform, unsigned int count) noexcept;
+
+    static inline void setUniformMat4(unsigned int shaderRef, const char* uniform_name, const glm::mat4& load_uniform) noexcept;
+    static inline void setUniformMat4Array(unsigned int shaderRef, const char* uniform_name, glm::mat4* load_uniform, unsigned int count) noexcept;
+
+#pragma endregion
+};
