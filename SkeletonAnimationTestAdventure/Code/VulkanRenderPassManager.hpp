1af7cb8f336b5ac74cd37409e56910ceb62ed4c3
Фаррах Фаттахов <farrakhfattahov@gmail.com>
2025-12-06 18:49:31 +0300

    update

---
+++ SkeletonAnimationTestAdventure/Code/VulkanRenderPassManager.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <array>
+#include <Volk/volk.h>
+
+#include "VulkanSwapchainManager.hpp"
+
+/* forward declaration */
+class VulkanDeviceManager;
+class VulkanSwapchainManager;
+
+class VulkanRenderPassManager {
+public:
+    VulkanRenderPassManager(VulkanDeviceManager* device_manager, VulkanSwapchainManager* swapchain_manager) : p_DeviceManager{ device_manager }, p_SwapchainManager{ swapchain_manager } {}
+    ~VulkanRenderPassManager() { this->Release(); }
+
+    void Release();
+    void CreateRenderPass();
+
+    VkRenderPass getRenderPass() const noexcept { return m_RenderPass; }
+
+private:
+    VulkanDeviceManager*    p_DeviceManager    = nullptr;
+    VulkanSwapchainManager* p_SwapchainManager = nullptr;
+
+    VkRenderPass m_RenderPass{};
+};
