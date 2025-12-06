4c1d81391ad4a835fcf371bcff7231157a9074c9
Фаррах Фаттахов <farrakhfattahov@gmail.com>
2025-12-06 18:55:13 +0300

    update

---
+++ SkeletonAnimationTestAdventure/Code/VulkanPipelineManager.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include <iostream>
+#include <optional>
+#include <array>
+#include <vector>
+#include <set>
+
+#include <Volk/volk.h>
+#include <GLFW/glfw3.h>
+
+#include "VulkanRenderMesh.h"
+
+/* forward declarations */
+class VulkanDeviceManager;
+class VulkanRenderPassManager;
+class VulkanSwapchainManager;
+class VulkanRenderMesh;
+
+class VulkanPipelineManager {
+public:
+    VulkanPipelineManager(VulkanDeviceManager* device_manager, VulkanRenderPassManager* render_pass_manager, VulkanSwapchainManager* swapchain_manager, VulkanRenderMesh* render_mesh)
+        : p_DeviceManager{ device_manager }, p_RenderPassManager{ render_pass_manager }, p_SwapchainManager{ swapchain_manager }, p_RenderMesh{ render_mesh } {}
+    ~VulkanPipelineManager() { this->Release(); }
+
+    void Release();
+
+    void CreateDescriptorSetLayout();
+    void CreateGraphicsPipeline();
+    void CreateDescriptorPool();
+    void CreateDescriptorSets();
+
+    VkPipelineLayout getPipelineLayout() const noexcept { return m_PipelineLayout; }
+    VkPipeline       getGraphicsPipeline() const noexcept { return m_GraphicsPipeline; }
+
+private:
+    static std::vector<char> readFile(const std::filesystem::path& path);
+    VkShaderModule           createShaderModule(const std::vector<char>& code) const;
+
+private:
+    VulkanDeviceManager*     p_DeviceManager     = nullptr;
+    VulkanRenderPassManager* p_RenderPassManager = nullptr;
+    VulkanSwapchainManager*  p_SwapchainManager  = nullptr;
+    VulkanRenderMesh*        p_RenderMesh        = nullptr;
+
+    VkDescriptorSetLayout m_DescriptorSetLayout{};
+    VkDescriptorPool      m_DescriptorPool{};
+
+    VkPipelineLayout m_PipelineLayout{};
+    VkPipeline       m_GraphicsPipeline{};
+};
